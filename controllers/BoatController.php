46abd1f90a078533fa275f855e84f3aaac60b728
PEDRO <cg3016455@LAB117M15.IFSP>
2022-06-06 17:43:30 -0300

    Refazendo O Back-End

---
+++ controllers/BoatController.php
@@ -0,0 +1,103 @@
+<?php
+class BoatController{
+
+    function create(){
+        $response = new Output();
+        $response->allowedMethod('POST');
+
+        $auth = new Auth();
+        $user_session = $auth->allowedRole('admin');
+
+        //Entradas
+        $img = $_POST['img'];
+        $name = $_POST['name'];
+        $price = $_POST['price'];
+        $tipo = $_POST['tipo'];
+        $estado = $_POST['estado'];
+        $ano_fab = $_POST['ano_fab'];
+        $tamanho = $_POST['tamanho'];
+        $trip = $_POST['trip'];
+        $local = $_POST['local'];
+        $comb = $_POST['comb'];
+
+        
+        //Processamento ou Persistencia
+        $boat = new Boat(null, $img, $name, $price, $tipo, $estado, $ano_fab, $tamanho, $trip, $local, $comb );
+        $id = $boat->create();
+        //Saída
+        $result['message'] = "Barco Cadastrado com sucesso!";
+        $result['product']['id'] = $id;
+        $result['product']['img'] = $img;
+        $result['product']['name'] = $name;
+        $result['product']['price'] = $price;
+        $result['product']['tipo'] = $tipo;
+        $result['product']['estado'] = $estado;
+        $result['product']['ano_fab'] = $ano_fab;
+        $result['product']['tamanho'] = $tamanho;
+        $result['product']['trip'] = $trip;
+        $result['product']['local'] = $local;
+        $result['product']['comb'] = $comb;
+        $response->out($result);
+    }
+
+    function delete(){
+        $response = new Output();
+        $response->allowedMethod('POST');
+
+        $auth = new Auth();
+        $user_session = $auth->allowedRole('admin');
+
+        $id = $_POST['id'];
+        $boat = new boat($id, null, null, null);
+        $boat->delete();
+        $result['message'] = "Barco deletado com sucesso!";
+        $result['boat']['id'] = $id;
+        $response->out($result);
+    }
+
+    function update(){
+        $response = new Output();
+        $response->allowedMethod('POST');
+
+        $auth = new Auth();
+        $user_session = $auth->allowedRole('admin');
+
+        $id = $_POST['id'];
+        $img = $_POST['img'];
+        $name = $_POST['name'];
+        $price = $_POST['price'];
+        $tipo = $_POST['tipo'];
+        $estado = $_POST['estado'];
+        $ano_fab = $_POST['ano_fab'];
+        $tamamho = $_POST['tamanho'];
+        $trip = $_POST['trip'];
+        $local = $_POST['local'];
+        $comb = $_POST['comb'];
+        $boat = new Boat($id, $img, $name, $price, $tipo, $estado, $ano_fab, $tamanho, $trip, $local, $comb );
+        $boat->update();
+        $result['message'] = "Barco atualizado com sucesso!";
+        $result['boat']['id'] = $id;
+        $result['boat']['name'] = $name;
+        $result['boat']['img'] = $img;
+        $response->out($result);
+    }
+
+    function selectAll(){
+        $response = new Output();
+        $response->allowedMethod('GET');
+        $boat = new Boat(null, null, null, null);
+        $result = $Boat->selectAll();
+        $response->out($result);
+    }
+
+    function selectById(){
+        $response = new Output();
+        $response->allowedMethod('GET');
+        $id = $_GET['id'];
+        $boat = new Boat($id, null, null, null);
+        $result = $boat->selectById();
+        $response->out($result);
+    }
+
+}
+?>
--- models/Product.php
+++ models/Product.php
@@ -1,28 +1,48 @@
 <?php 
 class Product{
     public $id;
-    public $photo;
-    public $title;
+    public $img;
+    public $name;
     public $price;
-    function __construct($id, $photo, $title, $price) {
+    public $tipo;
+    public $estado;
+    public $ano_fab;
+    public $tamanho;
+    public $trip;
+    public $local;
+    public $comb;
+    function __construct($id, $img, $name, $price, $tipo, $estado, $ano_fab, $tamanho, $trip, $local, $comb) {
         $this->id = $id;
-        $this->photo = $photo;
-        $this->title = $title;
+        $this->img = $img;
+        $this->name = $name;
         $this->price = $price;
+        $this->tipo = $tipo;
+        $this->estado = $estado;
+        $this->ano_fab = $ano_fab;
+        $this->tamanho = $tamanho;
+        $this->trip = $local;
+        $this->comb = $comb;
     }
     function create(){
         $db = new Database();
         try {
-            $stmt = $db->conn->prepare("INSERT INTO products (photo, title, price)
-            VALUES (:photo, :title, :price);");
-            $stmt->bindParam(':photo', $this->photo);
-            $stmt->bindParam(':title', $this->title);
+            $stmt = $db->conn->prepare("INSERT INTO boat ($id, $img, $name, $price, $tipo, $estado, $ano_fab, $tamanho, $trip, $local, $comb)
+            VALUES (:id, :img, `:name`, :price, :tipo, :estado, :ano_fab, :tamanho, `:local`, :comb);");
+            $stmt->bindParam(':id', $this->id);
+            $stmt->bindParam(':img', $this->img);
+            $stmt->bindParam(':name', $this->name);
             $stmt->bindParam(':price', $this->price);
+            $stmt->bindParam(':tipo', $this->tipo);
+            $stmt->bindParam(':estado', $this->estado);
+            $stmt->bindParam(':ano_fab', $this->ano_fab);
+            $stmt->bindParam(':tamanho',  $this->tamanho);
+            $stmt->bindParam(':local', $this->trip);
+            $stmt->bindParam(':comb', $this->comb);
             $stmt->execute();
             $id = $db->conn->lastInsertId();
             return $id;
         }catch(PDOException $e) {
-            $result['message'] = "Error Select All Products: " . $e->getMessage();
+            $result['message'] = "Error Select All Boats: " . $e->getMessage();
             $response = new Output();
             $response->out($result, 500);
         }
@@ -30,12 +50,12 @@ class Product{
     function delete(){
         $db = new Database();
         try {
-            $stmt = $db->conn->prepare("DELETE FROM products WHERE id = :id;");
+            $stmt = $db->conn->prepare("DELETE FROM boat WHERE id = :id;");
             $stmt->bindParam(':id', $this->id);
             $stmt->execute();
             return $stmt->rowCount();
         }catch(PDOException $e) {
-            $result['message'] = "Error Delete Product: " . $e->getMessage();
+            $result['message'] = "Error Delete Boat: " . $e->getMessage();
             $response = new Output();
             $response->out($result, 500);
         }
@@ -43,7 +63,7 @@ class Product{
     function update(){
         $db = new Database();
         try {
-            $stmt = $db->conn->prepare("UPDATE products SET photo = :photo, title = :title, price = :price WHERE id = :id;");
+            $stmt = $db->conn->prepare("UPDATE boat SET img = :img, `name` = `:name`, price = :price WHERE id = :id;");
             $stmt->bindParam(':id', $this->id);
             $stmt->bindParam(':photo', $this->photo);
             $stmt->bindParam(':title', $this->title);
